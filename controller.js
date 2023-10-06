d39a72cbb60f5339ef22c25fd7812243eb6ee828
Vitoria <gabrieli.vitoria@alunoescolassociais.org.br>
2023-10-06 09:52:16 -0300

    atualiza código fetch

--- controller.js
+++ controller.js
@@ -26,7 +26,7 @@ const pesquisarCep = async() =>{
 
     limparFormulario();
 
-    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
     if (cepValido(cep.value)){
         const dados = await fetch(url);
         const addres = await dados.json();
